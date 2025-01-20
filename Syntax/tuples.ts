a79a2d05e6b2d76a7526d811fc2966aab44bb2df
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-20 10:38:57 +0530

    [Raghav]: ADD - tuples practice

---
+++ Syntax/tuples.ts
@@ -0,0 +1,9 @@
+// in this , tuple is also there which means a fixed size of array and their dataype of each element is also defined.
+let arr : [null, number, string] = [null, 21, "hello"];
+console.log(arr);
+arr.push(52);
+console.log(arr);
+
+
+
+
