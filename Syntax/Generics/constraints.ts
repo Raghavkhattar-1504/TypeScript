8c4295bd2676f832df92867cfdee75324a546881
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-21 11:45:13 +0530

    [Raghav]: REFACTOR - constraints practice

--- Syntax/Generics/constraints.ts
+++ Syntax/Generics/constraints.ts
@@ -4,4 +4,4 @@ function getLength<T extends {length: number}>(item: T): number{
 
 console.log(getLength("Hello"))
 console.log(getLength([1,2,3]))
-// console.log(getLength(42))  throw error coz Number doesn't have a length property
+console.log(getLength(42));  
